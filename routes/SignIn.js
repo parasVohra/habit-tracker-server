6930b0254e1f8426c9672a6d601101a300a6ef45
Pursharth Vohra <pursharth01@gmail.com>
2021-05-16 23:01:46 -0400

    switch bcrypt with bcryptjs

--- routes/SignIn.js
+++ routes/SignIn.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models/users");
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.post("/", async (req, res) => {
   try {
--- routes/SignUp.js
+++ routes/SignUp.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users, validate } = require("../models/users");
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.post("/", async (req, res) => {
   try {

d2d8412423a95c244805d5ec2aeb95cb10465fd2
Pursharth Voha <pursharth.vohra@gmail.com>
2022-01-10 12:39:16 -0500

    implement https server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db_URI = config.get("mongoURI");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 //Routes Paths
 const Habits = require("./routes/Habits");
@@ -54,7 +56,22 @@ app.use("/api/updateIsTracked", UpdateIsTracked);
 app.use("/api/signUp", SignUp);
 app.use("/api/signIn", SignIn);
 
-const port = process.env.PORT || 6001;
-app.listen(port, () => {
-  console.log(`Server listening at port:${port}`);
-});
+if (process.env.NODE_ENV === "production") {
+  const options = {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/app.pursharthvohra.com/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/app.pursharthvohra.com/fullchain.pem"
+    ),
+  };
+
+  https.createServer(options, app).listen(443, () => {
+    console.log("Https server running on port 443");
+  });
+} else {
+  const port = process.env.PORT || 6001;
+  app.listen(port, () => {
+    console.log(`Server listening at port:${port}`);
+  });
+}

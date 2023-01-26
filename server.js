6ad9a9ed32b72656f9778122b8fdc60980ebb615
Pursharth Voha <pursharth.vohra@gmail.com>
2023-01-26 09:10:52 -0500

    server js move auth routes before auth middleware

--- server.js
+++ server.js
@@ -19,35 +19,37 @@ const UpdateHabitContent = require("./routes/UpdateHabitContent");
 
 //connect with mongodb
 mongoose
-  .connect(db_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("connected to mongoDb"))
-  .catch((err) => console.error(err));
+    .connect(db_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("connected to mongoDb"))
+    .catch((err) => console.error(err));
 mongoose.set("useCreateIndex", true);
 
 //enable CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "'GET, POST, OPTIONS, PUT, PATCH, DELETE'"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-auth-token, Authorization"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "'GET, POST, OPTIONS, PUT, PATCH, DELETE'"
+    );
+    next();
 });
 app.use(cors());
 
 //use middleware
 app.use(express.json());
+app.use("/api/signUp", SignUp);
+app.use("/api/signIn", SignIn);
 app.use(AuthMiddleware);
 
 app.get("/", (req, res) => {
-  res.status(200).send();
+    res.status(200).send();
 });
 
 //redirect routes to route folder
@@ -55,26 +57,24 @@ app.use("/api/saveHabit", Habits);
 app.use("/api/getHabits", GetHabits);
 app.use("/api/updateHabit", UpdateHabit);
 app.use("/api/updateIsTracked", UpdateIsTracked);
-app.use("/api/signUp", SignUp);
-app.use("/api/signIn", SignIn);
 app.use("/api/updateHabitContent", UpdateHabitContent);
 
 if (process.env.NODE_ENV === "production") {
-  const options = {
-    key: fs.readFileSync(
-      "/etc/letsencrypt/live/app.pursharthvohra.com/privkey.pem"
-    ),
-    cert: fs.readFileSync(
-      "/etc/letsencrypt/live/app.pursharthvohra.com/fullchain.pem"
-    ),
-  };
+    const options = {
+        key: fs.readFileSync(
+            "/etc/letsencrypt/live/app.pursharthvohra.com/privkey.pem"
+        ),
+        cert: fs.readFileSync(
+            "/etc/letsencrypt/live/app.pursharthvohra.com/fullchain.pem"
+        ),
+    };
 
-  https.createServer(options, app).listen(443, () => {
-    console.log("Https server running on port 443");
-  });
+    https.createServer(options, app).listen(443, () => {
+        console.log("Https server running on port 443");
+    });
 } else {
-  const port = process.env.PORT || 6001;
-  app.listen(port, () => {
-    console.log(`Server listening at port:${port}`);
-  });
+    const port = process.env.PORT || 6001;
+    app.listen(port, () => {
+        console.log(`Server listening at port:${port}`);
+    });
 }
